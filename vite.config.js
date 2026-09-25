325ff4e1c4a0fe37c58fd095966289fa7e4cb597
jeffersonramelo <jeffersonramelo@ufdpar.edu.br>
2026-09-25 12:04:41 -0300

    Ajusta Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Caminhos relativos funcionam tanto em domínios próprios quanto em
-  // projetos publicados em https://usuario.github.io/repositorio/.
-  base: './',
+  base: '/mvp/',
 });
